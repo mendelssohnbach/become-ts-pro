658351e4307adbe86273521d1266df0bdbb904cd
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2025-09-15 15:52:42 +0900

    ESLint ルールの設定を行う

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// eslint.config.js (Flat Config)
+import js from '@eslint/js';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      'no-restricted-syntax': [
+        'error',
+        {
+          // Enum の使用を禁止する
+          selector: 'TSEnumDeclaration',
+          message: 'Enums are not allowed. Use union types instead.',
+        },
+        {
+          // namespace の使用を禁止する
+          selector: 'TSModuleDeclaration',
+          message: 'Namespaces are not allowed. Use ES Modules instead.',
+        },
+      ],
+    },
+  },
+];
